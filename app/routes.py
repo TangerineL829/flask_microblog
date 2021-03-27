9c03d434144f70a1576d8e4ea0d608cab058d1fc
TangerineL829 <1282663969@qq.com>
2021-03-27 15:25:37 +0800

    two template finish

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
-from app import app#导入APP对象
-#定义路由
+from app import app
+from flask import render_template  # 从flask包中导入render_template函数
+
+
 @app.route('/')
 @app.route('/index')
-#视图函数
 def index():
-    return "hello world"
+    user = {'username': 'Miguel'}  # 用户
+    posts = [  # 创建一个列表：帖子。里面元素是两个字典，每个字典里元素还是字典，分别作者、帖子内容。
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
--- microblog.py
+++ microblog.py
@@ -2,5 +2,4 @@ from  app import  app
 
 
 if __name__ == '__main__':
-    print('git 12233')
     app.run(debug=True)
